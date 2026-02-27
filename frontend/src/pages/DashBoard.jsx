af50b22850088c0f9669cbbcb3a4d8014275c57e
Saurabh Yadav <yadavsaurabhrajepur@gmail.com>
2026-02-27 22:28:51 +0530

    Load tasks on dashboard

--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -1,3 +1,47 @@
+import { useEffect, useState } from "react";
+import API from "../services/api";
+import { useNavigate } from "react-router-dom";
+
 export default function Dashboard() {
-  return <h2>Dashboard</h2>;
+  const [tasks, setTasks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    try {
+      setLoading(true);
+      const res = await API.get("/tasks");
+      setTasks(res.data);
+    } catch (error) {
+      alert(error.response?.data?.message || "Failed to load tasks");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <button onClick={handleLogout}>Logout</button>
+
+      {loading ? (
+        <p>Loading tasks...</p>
+      ) : tasks.length > 0 ? (
+        tasks.map(t => (
+          <p key={t._id}>{t.title}</p>
+        ))
+      ) : (
+        <p>No tasks yet</p>
+      )}
+    </div>
+  );
 }
