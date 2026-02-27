5245d70a97b6f247db7493bc4e8b0a9b5312de3e
Saurabh Yadav <yadavsaurabhrajepur@gmail.com>
2026-02-27 22:53:06 +0530

    completed project

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import taskRoutes from "./routes/task.route.js";
@@ -7,6 +8,8 @@ import authRoutes from "./routes/auth.route.js";
 const app = express();
 dotenv.config();
 connectDB();
+
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
